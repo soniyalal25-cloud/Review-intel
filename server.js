086f34df7060a38f6dcaec20f7adbb118c02f2cd
soniyalal25-cloud <soniyalal25@gmail.com>
2026-05-04 14:57:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -184,7 +184,7 @@ http.createServer(async (req, res) => {
   }
 
   let filePath = req.url === '/' ? '/index.html' : req.url;
-  filePath = path.join(__dirname, 'public', filePath);
+  filePath = path.join(__dirname, filePath);
   fs.readFile(filePath, (err, data) => {
     if (err) { res.writeHead(404); res.end('Not found'); return; }
     res.writeHead(200, { 'Content-Type': MIME[path.extname(filePath)] || 'text/plain' });
